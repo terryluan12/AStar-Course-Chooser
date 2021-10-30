a76468c025436b40a322047868aac44fb0126d6c
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2021-10-30 16:51:50 -0400

    switch testing library from unittest to pytest and add gitignore file

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import pytest
+from minor import check_course_in_minor
+
+
+# Jean
+def test_check_course_in_minor():
+    course = "MIE439H1S"
+    minor = "Biomedical Engineering Minor"
+    result = check_course_in_minor(course)
+
+    assert result == minor
