57015a58371d932a3560e5df25d3588a7e82e8d1
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2021-10-31 02:22:17 -0400

    add starred function to course description

--- frontend/js/course_template.js
+++ frontend/js/course_template.js
@@ -1,5 +1,11 @@
+var wishlist = [];
+
 function starred(){
     document.getElementById('star').src = "img/starred.png"
+    var course_name = document.getElementById('course-title').textContent;
+    console.log(course_name);
+    wishlist.push(course_name);
+    console.log(wishlist[0]);
 }
 
 // pass in search result 
