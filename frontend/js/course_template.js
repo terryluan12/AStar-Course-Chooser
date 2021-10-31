3f61394dd84d9e980f310517b88607b297aa3926
Kuan-Chun (Jean) Lin <jeanlinkcl@gmail.com>
2021-10-31 01:48:45 -0400

    add javascript for linking syllabus to button

--- frontend/js/course_template.js
+++ frontend/js/course_template.js
@@ -1,3 +1,8 @@
 function starred(){
     document.getElementById('star').src = "img/starred.png"
 }
+
+// pass in search result 
+function get_syllabus(course="https://shuiblue.github.io/UofT-ECE444/") {
+    window.open(course);
+}
