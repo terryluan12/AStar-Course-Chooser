edd78a1c430d6b47be331e2d44a460ba60a3029d
Nick Woo <woocy50@gmail.com>
2021-11-17 22:18:02 -0500

    removed app.run() in app.py

--- AStar/app.py
+++ AStar/app.py
@@ -43,7 +43,7 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, extra_files=['app.py', 'controller.py', 'model.py'])
+    # app.run(host='0.0.0.0', port=5000, extra_files=['app.py', 'controller.py', 'model.py'])
     # with open("test.json") as f:
     #     data = json.load(f)
     # for i in range(75):
