fd7af143d8c3afb8daf5de03ebfb7056c50bbdc8
Terry <terryluan12@gmail.com>
2023-10-26 17:22:53 +0000

    * Fixed import issues with Link

--- AStar/frontend/src/components/CourseCard.js
+++ AStar/frontend/src/components/CourseCard.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./css/coursecard.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Col from 'react-bootstrap/Col'
-
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 // this function should:
 // receive  a list of courses called wishlist_data from the database
 // send modified comments to database. database saves it. the page rerenders to show the modified information
