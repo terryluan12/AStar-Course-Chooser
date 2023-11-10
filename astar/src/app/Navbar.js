00a9dae34fab7f60111bcc33547bd1f0e06fd593
Terry <terryluan12@gmail.com>
2023-11-10 17:49:51 +0000

    Removed main tags from _app.js Removed CourseDescription.js and ResultDisplay.jsx Migrated Navbar.js to a functional based component

--- astar/src/app/Navbar.js
+++ astar/src/app/Navbar.js
@@ -1,83 +1,75 @@
 'use client'
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/navbar.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from '../img/logo.png'
 import { Navbar, Nav } from "react-bootstrap";
 import Link from 'next/link'
 
-class NavbarComp extends Component {
+function NavbarComp(props) {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      username: undefined,
-      login: false
-    }
-  }
-  
+  const [username, setUsername] = useState(null)
+  const [login, setLogin] = useState(null)
 
-  componentDidMount() {
+  useEffect(() => {
     if (localStorage.getItem('username') !== "") {
-      this.setState({username: localStorage.getItem('username')})
+      setUsername(localStorage.getItem('username'))
     }
-  }
+  }, [])
 
-  logOut = () => {
+  const logOut = () => {
     localStorage.setItem('username', "");
-    this.setState({username: ""})
+      setUsername(localStorage.getItem(""))
   }
 
-  render() {
-    return (
-        <div>
-          <Navbar bg="myBlue" variant="dark" sticky="top" expand="lg">
-            <Navbar.Brand>
-              <img src={logo} alt="" />{" "}
-              <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
-                A* Course Finder
-              </Nav.Link>
-            </Navbar.Brand>
+  return (
+      <div>
+        <Navbar bg="myBlue" variant="dark" sticky="top" expand="lg">
+          <Navbar.Brand>
+            <img src={logo} alt="" />{" "}
+            <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
+              A* Course Finder
+            </Nav.Link>
+          </Navbar.Brand>
 
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-              <Nav>
-                <Nav.Link as={Link} href="/search">
-                  Search
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link as={Link} href="/search">
+                Search
+              </Nav.Link>
+
+              {username &&
+              <Nav.Link as={Link} href="/wishlist">
+              My Wishlist
+            </Nav.Link>
+
+            }
+
+
+              {!username ?
+                <Nav.Link as={Link} href="/login">
+                  Login
                 </Nav.Link>
-
-                {this.state.username &&
-                <Nav.Link as={Link} href="/wishlist">
-                My Wishlist
-              </Nav.Link>
-
+                :
+                <Nav.Link onClick={logOut} as={Link} href="/">
+                  Logout
+                </Nav.Link>
               }
 
 
-                {!this.state.username ?
-                  <Nav.Link as={Link} href="/login">
-                    Login
-                  </Nav.Link>
-                  :
-                  <Nav.Link onClick={this.logOut} as={Link} href="/">
-                    Logout
-                  </Nav.Link>
-                }
+              {!username &&
+              <Nav.Link as={Link} href="/signup">
+                Sign Up
+              </Nav.Link>
+              }
 
-
-                {!this.state.username &&
-                <Nav.Link as={Link} href="/signup">
-                  Sign Up
-                </Nav.Link>
-                }
-
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-    );
-  }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+  );
 }
 
 export default NavbarComp;
