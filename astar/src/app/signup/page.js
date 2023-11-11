3585c586c1243a8b24ab6817d2dd417b9535238e
Terry <terryluan12@gmail.com>
2023-11-11 00:54:37 +0000

    * Migrated SignUp to functional component * Changed SignUp to SignupPage

--- astar/src/app/signup/page.js
+++ astar/src/app/signup/page.js
@@ -1,81 +1,72 @@
 'use client'
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios"
 import '../../css/SignUp.css'
 
-class SignUp extends Component {
+function SignupPage(props) {
 
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: ""
-    };
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
   }
 
-  handleUsernameChange(event) {
-    this.setState({username: event.target.value})
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
   }
 
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value})
-  }
-
-  handleLogin(event) {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    this.createAccount(this.state.username, this.state.password)
+    const res = await createAccount(username, password)
+    switch(res.status){
+      case 200:
+        alert("Create Account Successfully!")
+        break
+      default:
+        alert("Unknown Error. Please contact website owner")
+        break
+    }
   }
 
-  createAccount = (username, password) => {
-    axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/register`, {
-        'username': this.state.username,
-        'password': this.state.password
-    })
-    .then(res => {
-        console.log("create status: ", res.status)
-        if (res.status === 200) {
-            alert("Create Account Successfully!")
-        }
-        
+  const createAccount = async (username, password) => {
+    return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/register`, {
+        'username': username,
+        'password': password
     })
   }
 
-  render() {
-    return (
-      <div className={"sign-up"}>
-        <h1> Sign Up</h1>
-        <form onSubmit={this.handleLogin}>
-          <input
-            name="username"
-            onChange={this.handleUsernameChange}
-            required
-            type="text"
-            placeholder="Username"
-            className={"signup-input"}
-          />
-          <br />
-          <input
-            name="password"
-            onChange={this.handlePasswordChange}
-            required
-            type="text"
-            placeholder="Password"
-            className={"signup-input"}
-          />
-          <br />
-          <button
-            type="submit"
-            className="signup-button"
-          >
-            Sign Up
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className={"sign-up"}>
+      <h1> Sign Up</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          name="username"
+          onChange={handleUsernameChange}
+          required
+          type="text"
+          placeholder="Username"
+          className={"signup-input"}
+        />
+        <br />
+        <input
+          name="password"
+          onChange={handlePasswordChange}
+          required
+          type="text"
+          placeholder="Password"
+          className={"signup-input"}
+        />
+        <br />
+        <button
+          type="submit"
+          className="signup-button"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SignUp;
+export default SignupPage;
