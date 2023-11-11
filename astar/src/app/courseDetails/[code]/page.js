77d184ec4ec5f8715dda06eb4bac732058531d21
Terry <terryluan12@gmail.com>
2023-11-11 03:04:06 +0000

    * Changed CourseDescription page to functional component

--- astar/src/app/courseDetails/[code]/page.js
+++ astar/src/app/courseDetails/[code]/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../css/course-description.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
@@ -13,223 +13,163 @@ import axios from "axios"
 
 let star = empty_star;
 
-class CourseDescriptionPage extends Component {
+function CourseDescriptionPage(props) {
 
-  constructor(props){
-    super(props)
+  const [course, setCourse] = useState({
+    course_code: props.params.code,
+    course_name: "",
+    division: "Faculty of Applied Science and Engineering",
+    department: "Department of Edward S. Rogers Sr. Dept. of Electrical & Computer Engineering",
+    graph : "",
+    course_description: "",
+    syllabus: "",
+    prerequisites: "",
+    corequisites: "",
+    exclusions: "",
+    starred: false,
+    graphics: [],
+    username: null
+  })
 
-    this.state = {
-      course_code: "",
-      course_name: "",
-      division: "Faculty of Applied Science and Engineering",
-      department: "Department of Edward S. Rogers Sr. Dept. of Electrical & Computer Engineering",
-      graph : "",
-      course_description: "",
-      syllabus: "",
-      prerequisites: "",
-      corequisites: "",
-      exclusions: "",
-      starred: false,
-      graphics: [],
-      username: undefined
-    }
+  const fetchCourse = async () => {
+    return await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/course/details?code=${course.course_code}`, {
+      code: course.course_code
+    })
   }
 
-
-
-  componentDidMount() {
-    console.log("pass in course code: ", this.props.params.code)
-    console.log("pass in course code: ", this.code)
-
-    axios.get(`${process.env.NEXT_PUBLIC_API_URL}/course/details?code=${this.props.params.code}`, {
-      code: this.props.course_code
-    })
-      .then(res => {
-        console.log(res.data)
-        this.setState({course_code: res.data.course.code})
-        this.setState({course_name: res.data.course.name})
-        this.setState({course_description : res.data.course.description})
-        this.setState({graph: res.data.course.graph})
-        let prereq_len = res.data.course.prereq.length
-        if (prereq_len > 1) {
-          let prereq_str = ""
-          for (let i = 0; i < prereq_len; i++) {
-            prereq_str += res.data.course.prereq[i]
-            if (i !== prereq_len - 1) {
-              prereq_str += ", "
-            }
-          }
-          this.setState({prerequisites : prereq_str})
-        } else {
-          this.setState({prerequisites : res.data.course.prereq})
-        }
-        let coreq_len = res.data.course.coreq.length
-        if (coreq_len > 1) {
-          let coreq_str = ""
-          for (let i = 0; i < coreq_str; i++) {
-            coreq_str += res.data.course.coreq[i]
-            if (i !== coreq_len - 1) {
-              coreq_str += ", "
-            }
-          }
-          this.setState({corequisites : coreq_str})
-        } else {
-          this.setState({corequisites : res.data.course.coreq})
-        }
-        let exclusion_len = res.data.course.exclusion.length
-        if (exclusion_len > 1) {
-          let exclusion_str = ""
-          for (let i = 0; i < exclusion_str; i++) {
-            exclusion_str += res.data.course.exclusion[i]
-            if (i !== exclusion_len - 1) {
-              exclusion_str += ", "
-            }
-          }
-          this.setState({exclusions : exclusion_str})
-        } else {
-          this.setState({exclusions : res.data.course.exclusion})
-        }
-        let syllabus_link = "http://courses.skule.ca/course/" + this.props.params.code
-        this.setState({syllabus : syllabus_link})
-
-        let temp_graph = []
-        //temp_graph.push(<ShowGraph graph_src={this.state.graph}></ShowGraph>)
-        this.setState({graphics: temp_graph})
-
-
-    })
-    if(this.state.username){
-      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist?username=${this.state.username}`)
-      .then(res => {
-        console.log(`res is ${res}`)
-        let len = res.data.wishlist.course.length
-        for (let i = 0; i < len; i++) {
-          console.log("checking: ", res.data.wishlist.course[i].code)
-          console.log("course: ", this.props.params.code)
-          if (res.data.wishlist.course[i].code === this.props.params.code) {
-            star = starred
-            this.setState({starred: true})
-
-          }
-        }
+  const fetchWishlist = async () => {
+      return await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist?username=${course.username}`)
+  }
+  const toggleStar = async () => {
+    if (!starred){
+      return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist/addCourse?username=${course.username}&code=${course.course_code}`, {
+        'code': course.course_code, 'username':course.username
+      })
+    }
+    else{
+      return await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist/removeCourse?username=${course.username}&code=${course.course_code}`, {
+        'code': course.course_code, 'username':course.username
       })
     }
 
-
-
-    console.log("new state: ", this.state)
   }
 
-  check_star = () => {
+  useEffect(() => {
+    const setCoursePage = async () => {
+      const res = await fetchCourse()
+      const prereq_str = res.data.course.prereq.join(",")
+      const coreq_str = res.data.course.coreq.join(",")
+      const exclusion_str = res.data.course.exclusion.join(",")
 
-    if(this.state.username){
-      if (this.state.starred === false) { //if user is logged in, add to course
+      let syllabus_link = "http://courses.skule.ca/course/" + course.course_code
 
-        //add course to wishlist
-        console.log("username/code", this.state)
-        axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist/addCourse?username=${this.state.username}&code=${this.state.course_code}`, {
-          'code': this.state.course_code, 'username':this.state.username
-        })
-        .then(resp =>{
-          if(resp.status === 200){
-            console.log("successfully added course and starred")
-            star = starred;
-            this.setState({starred: true});
+      let temp_graph = []
+      //temp_graph.push(<ShowGraph graph_src={graph}></ShowGraph>)
 
-          }else{
-            console.log("error occured while modifying wishlist: ", resp.status)
-          }
-        })
-      } else {
+      setCourse({
+        ...course,
+        course_name: res.data.course.name,
+        graph: res.data.course.graph,
+        course_description : res.data.course.description, 
+        syllabus: syllabus_link,
+        prerequisites : prereq_str,
+        corequisites : coreq_str,
+        exclusions: exclusion_str,
+        graphics: temp_graph
+      })
+    }
+    const setWishlist = async () => {
+      const res = await fetchWishlist()
+      isStarred = res.data.wishlist.some(course => course.course_code === searchParams.get("code"))
+      setCourse({...course, starred: isStarred})
+        }
+    setCoursePage()
+      .catch(console.error)
+    if(course.username)
+        setWishlist().catch(console.error)
 
-        //remove course from wishlist
-        axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/wishlist/removeCourse?username=${this.state.username}&code=${this.state.course_code}`, {
-          'code': this.state.course_code, 'username':this.state.username
-        })
-        .then(resp =>{
-          if(resp.status === 200){
-            console.log("successfully REMOVED course and unstarred")
-            star = empty_star;
-            this.setState({starred: false});
-          }else{
-            console.log("error occured while modifying wishlist: ", resp.status)
-          }
-        })
+  }, [])
+
+  const check_star = async () => {
+
+    if(course.username){
+      const res = await toggleStar()
+      setCourse({...course, starred: !course.starred});
+      star = (star == empty_star) ? starred : empty_star
+      if(res.status != 200){
+        console.log("error occured while modifying wishlist: ", resp.status)
       }
-
     }else{ //else, notify
       alert("You must login to save a course.")
     }
   }
 
-  openLink = () => {
-    const newWindow = window.open(this.state.syllabus, '_blank', 'noopener,noreferrer');
+  const openLink = () => {
+    const newWindow = window.open(course.syllabus, '_blank', 'noopener,noreferrer');
     if (newWindow) {
       newWindow.opener = null;
     }
   }
 
-	render() {
-		return(
+  return(
 
-      <div className="page-content">
-        <Container className="course-template">
-          <Row float="center" className="course-title">
-            <Col xs={8}>
-              <h1>{this.state.course_code} : {this.state.course_name}</h1>
+    <div className="page-content">
+      <Container className="course-template">
+        <Row float="center" className="course-title">
+          <Col xs={8}>
+            <h1>{course.course_code} : {course.course_name}</h1>
+          </Col>
+          <Col xs={4}>
+            <img src={star} onClick={course.check_star} alt="" />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="col-item">
+            <h3>Division</h3>
+            <p>{course.division}</p>
+          </Col>
+          <Col className="col-item">
+            <h3>Department</h3>
+            <p>{course.department}</p>
+          </Col>
+          <Col className="col-item">
+            <h3>Past Tests and Syllabi</h3>
+            <button className={"syllabus-link"} onClick={openLink}>View</button>
+          </Col>
+        </Row>
+        <Row className="col-item course-description">
+          <h3>Course Description</h3>
+          <p>{course.course_description}</p>
+        </Row>
+        <Row className="col-item course-requisite">
+          <Row>
+            <h3>Course Requisites</h3>
+          </Row>
+          <Row>
+            <Col className="requisites-display">
+              <h4>Pre-Requisites</h4>
+              <p>{course.prerequisites}</p>
             </Col>
-            <Col xs={4}>
-              <img src={star} onClick={this.check_star} alt="" />
+            <Col className="requisites-display">
+              <h4>Co-Requisites</h4>
+              <p>{course.corequisites}</p>
+            </Col>
+            <Col className="requisites-display">
+              <h4>Exclusion</h4>
+              <p>{course.exclusions}</p>
             </Col>
           </Row>
           <Row>
-            <Col className="col-item">
-              <h3>Division</h3>
-              <p>{this.state.division}</p>
-            </Col>
-            <Col className="col-item">
-              <h3>Department</h3>
-              <p>{this.state.department}</p>
-            </Col>
-            <Col className="col-item">
-              <h3>Past Tests and Syllabi</h3>
-              <button className={"syllabus-link"} onClick={this.openLink}>View</button>
-            </Col>
+            <div className={"req-graph"}>
+              <img style={{width: "70%", marginBottom: "3%"}} alt="" src={requisite_label}></img>
+              <img src={`data:image/jpeg;base64,${course.graph}`} alt="" ></img>
+            </div>
           </Row>
-          <Row className="col-item course-description">
-            <h3>Course Description</h3>
-            <p>{this.state.course_description}</p>
-          </Row>
-          <Row className="col-item course-requisite">
-            <Row>
-              <h3>Course Requisites</h3>
-            </Row>
-            <Row>
-              <Col className="requisites-display">
-                <h4>Pre-Requisites</h4>
-                <p>{this.state.prerequisites}</p>
-              </Col>
-              <Col className="requisites-display">
-                <h4>Co-Requisites</h4>
-                <p>{this.state.corequisites}</p>
-              </Col>
-              <Col className="requisites-display">
-                <h4>Exclusion</h4>
-                <p>{this.state.exclusions}</p>
-              </Col>
-            </Row>
-            <Row>
-              <div className={"req-graph"}>
-                <img style={{width: "70%", marginBottom: "3%"}} alt="" src={requisite_label}></img>
-                <img src={`data:image/jpeg;base64,${this.state.graph}`} alt="" ></img>
-              </div>
-            </Row>
-          </Row>
-        </Container>
-      </div>
+        </Row>
+      </Container>
+    </div>
 
-		)
-	}
+  )
 }
 
 export default CourseDescriptionPage
