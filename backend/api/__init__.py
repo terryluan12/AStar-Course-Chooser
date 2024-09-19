9a809375ee5383f68bdf35b5897ffaee4c077cdc
Terry <terryluan12@gmail.com>
2024-09-18 21:12:46 -0400

    * Changed __init__.py file from a single use file to a factory function

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -9,35 +9,33 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config["DEBUG"] = os.environ.get("DEBUG", default=True)
-app.config["TESTING"] = os.environ.get("TESTING", default=True)
-# Configure MongoEngine
-app.config["MONGODB_HOST"] = os.environ.get("MONGODB_HOST", default=True)
-# Configure SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "SQLALCHEMY_DATABASE_URI", default=True
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["DEBUG"] = os.environ.get("DEBUG", default=True)
+    app.config["TESTING"] = os.environ.get("TESTING", default=True)
+    # Configure MongoEngine
+    app.config["MONGODB_HOST"] = os.environ.get("MONGODB_HOST", default=True)
+    # Configure SQLAlchemy
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI", default=True
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-CORS(app)
+    CORS(app)
 
-connect(host=app.config["MONGODB_HOST"])
+    connect(host=app.config["MONGODB_HOST"])
 
-sql_db.init_app(app)
-with app.app_context():
-    sql_db.create_all()
+    sql_db.init_app(app)
+    with app.app_context():
+        sql_db.create_all()
 
+    api.init_app(app)
 
-
-api.init_app(app)
-
-
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + "/" + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, "index.html")
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + "/" + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, "index.html")
